a96abe94dc9375635c77c56ad96dbb8905f75b03
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-09-09 03:51:26 +0100

    adding .env

--- src/DataFetching/index.js
+++ src/DataFetching/index.js
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 var qs = require('qs');
+require('dotenv').config();
+
+console.log(process.env.API_TOKEN)
 
 function DataFetching() {
+
     const [company, setCompany] = useState([]);
     const [departure, setDeparture] = useState([]);
     const [arrival, setArrival] = useState([]);
     const [currency, setCurrency] = useState([]);
     const [total, setTotal] = useState([]);
 
-
     useEffect(() => {
-        const originLocationCode = "SYD"
-        const destinationLocationCode = "BKK"
+        const originLocationCode = "LON"
+        const destinationLocationCode = "GRU"
         const departureDate = "2020-09-10"
         const returnDate = "2020-09-12"
         const adultsMax = "2"
 
         var data = qs.stringify({
-            'Authorization': 'Bearer rsbzSuAgySFnEA1VCvKOvdMdWx1c'
+            'Authorization': `Bearer l9pOz7GEN1WWzbnhcUWd7VW4wNOZ`
         });
 
         var config = {
             method: 'get',
             url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDate=${departureDate}&returnDate=${returnDate}&adults=2&max=${adultsMax}`,
             headers: {
-                'Authorization': 'Bearer rsbzSuAgySFnEA1VCvKOvdMdWx1c',
+                'Authorization': `Bearer l9pOz7GEN1WWzbnhcUWd7VW4wNOZ`,
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             data: data
@@ -33,15 +36,12 @@ function DataFetching() {
         axios(config)
             .then(response => {
                 console.log(response);
-                const companyName = response.data.dictionaries.carriers.CI;
+                const companyName = response.data.dictionaries.carriers;
                 const departureDateAndTime = response.data.data[0].itineraries[0].segments[0].departure.at;
                 const arrivalDateAndTime = response.data.data[0].itineraries[0].segments[0].arrival.at;
                 const currencyType = response.data.data[0].price.currency;
                 const totalToPay = response.data.data[0].price.total;
-                console.log(companyName);
-                console.log(arrivalDateAndTime);
-                console.log(departureDateAndTime);
-                console.log(currency, total);
+
                 setCompany(JSON.stringify(companyName));
                 setDeparture(JSON.stringify(arrivalDateAndTime));
                 setArrival(JSON.stringify(departureDateAndTime));
@@ -52,7 +52,6 @@ function DataFetching() {
                 console.log(error);
             });
     }, [currency, total])
-
 
     return (
         <div style={{ textAlign: "center" }}>
