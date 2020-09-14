19a131ee2718812d54bae10e9df3b4152847337b
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-09-14 22:57:16 +0100

    Fetch bug fixef and card render after the query

--- src/DataFetching/index.js
+++ src/DataFetching/index.js
@@ -4,7 +4,8 @@ import ResultBox from '../components/ResultBox';
 var qs = require('qs');
 require('dotenv').config();
 
-const myToken = process.env.REACT_APP_API_TOKEN
+const myToken = 'n4mvk8rC0m8zz0G0KxUn88F7RzqS'
+//process.env.REACT_APP_API_TOKEN
 console.log(myToken);
 
 
@@ -18,37 +19,43 @@ function DataFetching({ originLocationCode, destinationLocationCode, departureDa
 
     useEffect(() => {
 
-        var data = qs.stringify({
-            'Authorization': `Bearer ${myToken}`
-        });
-
-        var config = {
-            method: 'get',
-            url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDate=${departureDate}&returnDate=${returnDate}&adults=2&max=${passengerQuantity}`,
-            headers: {
-                'Authorization': `Bearer ${myToken}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            data: data
-        };
-        axios(config)
-            .then(response => {
-                console.log(response);
-                const companyName = response.data.dictionaries.carriers;
-                const departureDateAndTime = response.data.data[0].itineraries[0].segments[0].departure.at;
-                const arrivalDateAndTime = response.data.data[0].itineraries[0].segments[0].arrival.at;
-                const currencyType = response.data.data[0].price.currency;
-                const totalToPay = response.data.data[0].price.total;
-
-                setCompany(JSON.stringify(companyName));
-                setDeparture(JSON.stringify(arrivalDateAndTime));
-                setArrival(JSON.stringify(departureDateAndTime));
-                setCurrency(JSON.stringify(currencyType));
-                setTotal(JSON.parse(totalToPay));
-            })
-            .catch(function (error) {
-                console.log(error);
+        const fetchFunction = async () => {
+            var data = qs.stringify({
+                'Authorization': `Bearer ${myToken}`
             });
+
+            var config = {
+                method: 'get',
+                url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDate=${departureDate}&returnDate=${returnDate}&adults=2&max=${passengerQuantity}`,
+                headers: {
+                    'Authorization': `Bearer ${myToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                data: data
+            };
+
+            if (passengerQuantity.length > 0) {
+                await axios(config)
+                    .then(response => {
+                        console.log(response);
+                        const companyName = response.data.dictionaries.carriers;
+                        const departureDateAndTime = response.data.data[0].itineraries[0].segments[0].departure.at;
+                        const arrivalDateAndTime = response.data.data[0].itineraries[0].segments[0].arrival.at;
+                        const currencyType = response.data.data[0].price.currency;
+                        const totalToPay = response.data.data[0].price.total;
+
+                        setCompany(JSON.stringify(companyName));
+                        setDeparture(JSON.stringify(arrivalDateAndTime));
+                        setArrival(JSON.stringify(departureDateAndTime));
+                        setCurrency(JSON.stringify(currencyType));
+                        setTotal(JSON.parse(totalToPay));
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        }
+        fetchFunction()
     }, [originLocationCode, destinationLocationCode, departureDate, returnDate, passengerQuantity])
 
     return (
