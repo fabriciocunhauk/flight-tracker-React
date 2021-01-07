f3eb80332abb48f2a2b369c6e489a13a7a93a330
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2021-01-07 03:00:06 +0000

    added switch

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from '../src/pages/LandingPage/index';
 import ResultPage from './pages/ResultPage/ResultPage';
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/result" component={ResultPage} />
+            <Switch>
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/result" component={ResultPage} />
+            </Switch>
         </BrowserRouter>
     );
 }
